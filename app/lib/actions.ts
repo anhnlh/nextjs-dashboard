3e4e567caa47a28a1ef48b38df1748c814166e76
anhnlh <aln4739@rit.edu>
2024-01-28 21:45:58 -0500

    forgot to redirect after creating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,6 +31,7 @@ export async function createInvoice(formData: FormData) {
     `;
 
   revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
